3899115a19c43f9ab021b1be2ca4bab7a5e7959a
EugeneBashorin <bashorineugene@gmail.com>
2022-12-13 18:11:42 +0200

    added Profile component and styles for component

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,49 @@
+import PropTypes from "prop-types";
+import css from "./Profile.module.css";
+
+export const Profile = ({
+    avatar = "../../../../public/logo192.png",
+    name,
+    tag,
+    location,
+    stats: {followers, views, likes}
+    }) =>(
+    
+    <div className={css.profile}>
+        <div className={css.description}>
+            <img
+                src={avatar}
+                alt="User avatar"
+                className={css.avatar}
+            />
+            <p className={css.name}>{name}</p>
+            <p className={css.tag}>@{tag}</p>
+            <p className={css.location}>{location}</p>
+        </div>
+
+        <ul className={css.stats}>
+            <li className={css.stats_item}>
+            <span className="label">Followers</span>
+            <span className="quantity">{followers}</span>
+            </li>
+            <li className={css.stats_item}>
+            <span className="label">Views</span>
+            <span className="quantity">{views}</span>
+            </li>
+            <li className={css.stats_item}>
+            <span className="label">Likes</span>
+            <span className="quantity">{likes}</span>
+            </li>
+        </ul>
+    </div>
+);
+
+Profile.propTypes = {
+    avatar: PropTypes.string,
+    name: PropTypes.string,
+    tag:PropTypes.string,
+    location: PropTypes.string,
+    followers: PropTypes.number,
+    views: PropTypes.number,
+    likes: PropTypes.number,
+}
